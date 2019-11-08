98f5f53b04cdf799b946d804eb0e81f5450b5f7f
czc <a1321573686@163.com>
2019-11-08 11:34:30 +0800

    首次上传git，项目为新建空项目

---
+++ app/src/main/java/com/team15/picture_manager/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.team15.picture_manager.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.team15.picture_manager.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
