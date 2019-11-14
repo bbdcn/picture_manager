1695297289f7e597b88c8809bafa546db0ec0b23
czc <a1321573686@163.com>
2019-11-14 20:20:55 +0800

    完成了所有页面的基础编辑以及界面间的跳转

--- app/src/main/java/com/team15/picture_manager/ui/FavoriteFragment.java
+++ app/src/main/java/com/team15/picture_manager/ui/FavoriteFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.team15.picture_manager.R;
 
@@ -15,9 +17,16 @@ public class FavoriteFragment extends Fragment {
     public FavoriteFragment(){
     }
 
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_favorite, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageView imageView_favorite_1 = view.findViewById(R.id.imageView_favorite_1);
+        imageView_favorite_1.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_checkFragment));
     }
 }
--- app/src/main/java/com/team15/picture_manager/ui/PhotosFragment.java
+++ app/src/main/java/com/team15/picture_manager/ui/PhotosFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.team15.picture_manager.R;
 
@@ -18,5 +20,13 @@ public class PhotosFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_photos, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageView imageView = view.findViewById(R.id.imageView_photos_1);
+        imageView.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_checkFragment));
     }
 }
