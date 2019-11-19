bf01a4ffeea6a94753dae2da4354fc45a4354a5d
bbdcn <1321573686@qq.com>
2019-11-19 10:17:39 +0800

    简化了界面导航，新建了几个包

--- app/src/main/java/com/team15/picture_manager/ui/albums_fragment/AlbumsFragment.java
+++ app/src/main/java/com/team15/picture_manager/ui/albums_fragment/AlbumsFragment.java
@@ -2,8 +2,6 @@ package com.team15.picture_manager.ui.albums_fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -11,7 +9,6 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
